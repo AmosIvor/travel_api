39e066dfab7c6fb281ceb8df50a81ab64f8fdfea
AmosIvor <trantuanvu00000@gmail.com>
2024-05-26 16:41:57 +0700

    [CONFIG] add middleware to handle exception

---
+++ travel_api/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Text.Json;
+using travel_api.Exceptions;
+
+namespace travel_api.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            HttpStatusCode status;
+            string message = "";
+
+            var exceptionType = ex.GetType();
+
+            if (exceptionType == typeof(NotFoundException))
+            {
+                message = ex.Message;
+                status = HttpStatusCode.NotFound;
+            }
+            else if (exceptionType == typeof(DuplicateValueException))
+            {
+                message = ex.Message;
+                status = HttpStatusCode.Conflict;
+            } 
+            else
+            {
+                message = ex.Message;
+                status = HttpStatusCode.InternalServerError;
+            }
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = message, status });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+
+            return context.Response.WriteAsync(exceptionResult);
+        }
+    }
+}
